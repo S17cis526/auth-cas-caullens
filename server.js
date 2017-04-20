8cbe0512e0f30b59006acb386d850bdbd2d9f7af
caullens <caullens@ksu.edu>
2017-04-20 14:20:48 -0500

    CAS Auth

--- server.js
+++ server.js
@@ -3,18 +3,23 @@
  */
 
 // Constants
-const PORT = 3000;
+const PORT = 5500;
 
 // Requires
 var fs = require('fs');
 var http = require('http');
 var express = require('express');
+var config = require('./config.json');
 
 // The Express app
 var app = express();
 
 // Serve files from public folder
 app.use(express.static('public'));
+
+// Set up authentication
+var authCAS = require('auth-cas');
+var auth = new authCAS(config.host, config.casHost);
 
 // Start the server
 app.listen(PORT, function(){
